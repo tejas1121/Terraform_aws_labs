0ceddaa8fcd597662bcfb829368d38d54cfa7235
Tejas <tejasburkul967@gmail.com>
2026-04-22 19:01:39 +0530

    dyanamically created 2 ec2 with diffrent ami

--- tf-multi-resources/ec2.tf
+++ tf-multi-resources/ec2.tf
@@ -1,19 +1,11 @@
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
 
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
 resource "aws_instance" "my_ec2" {
-    ami = data.aws_ami.amazon_linux.id
+    count = 2
+    ami = count.index <1 ? "ami-0e12ffc2dd465f6e4" : "ami-05d2d839d4f73aafb"
     instance_type = var.instance_type
-    count = 4
-    subnet_id = count.index < 2 ? aws_subnet.main[0].id : aws_subnet.main[1].id
+  
+    subnet_id = count.index < 1 ? aws_subnet.main[0].id : aws_subnet.main[1].id
    
     tags = {
         Name = "my-ec2-instance-${count.index + 1}"
