4fb7ecfb7123179732c986458f3a15a3c4bc5879
Tejas <tejasburkul967@gmail.com>
2026-04-22 19:05:44 +0530

    alternative method for same task

--- tf-multi-resources/ec2.tf
+++ tf-multi-resources/ec2.tf
@@ -2,7 +2,8 @@
 
 resource "aws_instance" "my_ec2" {
     count = 2
-    ami = count.index <1 ? "ami-0e12ffc2dd465f6e4" : "ami-05d2d839d4f73aafb"
+    ami = element(["ami-0e12ffc2dd465f6e4", "ami-05d2d839d4f73aafb"], count.index)
+   // ami = count.index <1 ? "ami-0e12ffc2dd465f6e4" : "ami-05d2d839d4f73aafb"
     instance_type = var.instance_type
   
     subnet_id = count.index < 1 ? aws_subnet.main[0].id : aws_subnet.main[1].id
